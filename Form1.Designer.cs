20ca870ede660618b26eec4bcba26148366a5dae
Austin Herman <46000890+austeeen@users.noreply.github.com>
2019-02-08 12:40:45 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace bitmapproto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.load = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fun = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // load
+            // 
+            this.load.Location = new System.Drawing.Point(8, 688);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(75, 23);
+            this.load.TabIndex = 0;
+            this.load.Text = "load";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(8, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(778, 678);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fun
+            // 
+            this.fun.Location = new System.Drawing.Point(88, 688);
+            this.fun.Name = "fun";
+            this.fun.Size = new System.Drawing.Size(75, 23);
+            this.fun.TabIndex = 1;
+            this.fun.Text = "do fun";
+            this.fun.UseVisualStyleBackColor = true;
+            this.fun.Click += new System.EventHandler(this.fun_click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(167, 688);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 2;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 723);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.fun);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.load);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button fun;
+        private System.Windows.Forms.Button save;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace bitmapproto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void load_click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFD = new OpenFileDialog())
+            {
+                // File dialog settings
+                openFD.Title = "Select an image file";
+                openFD.InitialDirectory = "C:\\Users\\100057822\\Desktop";
+                openFD.Filter = "bmp files (*.bmp)|*.bmp";
+                openFD.RestoreDirectory = true;
+
+                DialogResult result = openFD.ShowDialog();
+
+                if (result == DialogResult.OK)
+                    bmppath = openFD.FileName;
+
+                try {
+                    bmp = new Bitmap(Image.FromFile(bmppath));
+                    pictureBox1.Image = bmp;
+                }
+                catch(Exception)
+                { }
+               
+            }
+        }
+
+        private void fun_click(object sender, EventArgs e)
+        {
+            if (bmp == null) return;
+
+            Color floor = Color.FromArgb(255, 0, 255);
+            Color floorchange = Color.FromArgb(255, 255, 255);
+
+            int low       = coltoi(Color.FromArgb(0, 0, 255));
+            int lowmed    = coltoi(Color.FromArgb(0, 255, 255));
+            int med       = coltoi(Color.FromArgb(0, 255, 0));
+            int medhigh   = coltoi(Color.FromArgb(255, 255, 0));
+            int high      = coltoi(Color.FromArgb(255, 0, 0));
+
+            //string csvheader = "x, int";
+            //StreamWriter csv = new StreamWriter(Path.GetDirectoryName(bmppath) + "\\" + Path.GetFileNameWithoutExtension(bmppath) + ".csv");
+
+            //csv.WriteLine(csvheader);
+
+            // Loop through the images pixels to reset color.
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color pixel = bmp.GetPixel(x, y);
+
+                    if (pixel == floor)
+                        bmp.SetPixel(x, y, floorchange);
+                    
+                    //Console.WriteLine(coltoi(bmp.GetPixel(x, y)));
+                    //csv.WriteLine(x + "," + coltoi(bmp.GetPixel(x, y)));
+
+                }
+            }
+
+            //csv.Close();
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void save_click(object sender, EventArgs e)
+        {
+            // Get the file's save name
+            SaveFileDialog saveFD = new SaveFileDialog();
+
+            saveFD.Title = "Save the image file";
+            saveFD.Filter = "bmp files (*.bmp)|*.bmp";
+
+            saveFD.FileName = Path.GetFileNameWithoutExtension(bmppath);
+
+            saveFD.InitialDirectory = Path.GetDirectoryName(bmppath);
+
+            DialogResult result = saveFD.ShowDialog();
+
+            if (result == DialogResult.OK)
+                bmp.Save(saveFD.FileName);
+            else
+                MessageBox.Show("File was not saved.", "File Not Saved", 0);
+        }
+
+        private int coltoi(Color pixel)
+        {
+            int numcolor = 0;
+            int greenShift = 510;
+
+            if (pixel.R == 255)
+                numcolor = pixel.R + (255 - pixel.G) + pixel.B + greenShift;
+            else if (pixel.G == 255)
+                numcolor = pixel.R + pixel.G + (255 - pixel.B);
+            else
+                numcolor = pixel.R + pixel.G + pixel.B - 255;
+
+            return numcolor;
+
+        }
+
+        public Bitmap bmp = null;
+        public string bmppath = "";
+    }
+}
